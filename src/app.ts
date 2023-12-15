b2b0612e29c54741c267de1eda126592b2069563
Chivan9406 <ivan9406@hotmail.com>
2023-12-14 19:10:48 -0600

    Implement Postgres log data source and service check
    Added a new file, `check-service-multiple.ts` in the use-cases directory for checking service status. Also, implemented the `PostgresLogDatasource` for logging into a Postgres database. Updated Prisma schema to include log model and server file to use the newly implemented services. Ensured files related to Prisma migrations are ignored in `.gitignore`.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {Server} from './presentation/server'
 import {LogModel, MongoDatabase} from './data/mongo'
 import {envs} from './config/plugins/envs.plugin'
+import {PrismaClient} from '@prisma/client'
 
 (async () => {
     main()
@@ -11,6 +12,23 @@ async function main() {
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME,
     })
+
+    // const prisma = new PrismaClient()
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         message: 'Test message desde Prisma',
+    //         origin: 'App.ts',
+    //         level: 'HIGH',
+    //     }
+    // })
+    // console.log({newLog})
+    //
+    // const logs = await prisma.logModel.findMany({
+    //     where: {
+    //         level: 'HIGH',
+    //     }
+    // })
+    // console.log(logs)
 
     // const newLog = await LogModel.create({
     //     message: 'Test message desde Mongo',
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,11 +2,22 @@ import {LogRepositoryImpl} from '../infrastructure/repositories/log.repository.i
 import {FileSystemDatasource} from '../infrastructure/datasources/file-system.datasource'
 import {EmailService} from './email/email.service'
 import {LogSeverityLevel} from '../domain/entities/log.entity'
+import {MongoLogDatasource} from '../infrastructure/datasources/mongo-log.datasource'
+import {PostgresLogDatasource} from '../infrastructure/datasources/postgres-log.datasource'
+import {CheckService} from '../domain/use-cases/checks/check-service'
+import {CronService} from './cron/cron.service'
+import {CheckServiceMultiple} from '../domain/use-cases/checks/check-service-multiple'
 
 const logRepository = new LogRepositoryImpl(
-    new FileSystemDatasource()
+    // new FileSystemDatasource()
     // new MongoLogDatasource()
+    new PostgresLogDatasource()
 )
+
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource())
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource())
+const postgresLogRepository = new LogRepositoryImpl(new PostgresLogDatasource())
+
 const emailService = new EmailService()
 
 export class Server {
@@ -41,6 +52,19 @@ export class Server {
         //
         //         new CheckService(
         //             logRepository,
+        //             () => console.log(`${url} is ok`),
+        //             (error) => console.error(error)
+        //         ).execute(url)
+        //     }
+        // )
+
+        // CronService.createJob(
+        //     '*/5 * * * * *',
+        //     () => {
+        //         const url = 'https://google.com'
+        //
+        //         new CheckServiceMultiple(
+        //             [fsLogRepository, mongoLogRepository, postgresLogRepository],
         //             () => console.log(`${url} is ok`),
         //             (error) => console.error(error)
         //         ).execute(url)
