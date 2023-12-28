0834c094339a120d0af9db67231e6b5c55c15d5b
Chivan9406 <ivan9406@hotmail.com>
2023-12-28 16:31:12 -0600

    mongo-log.datasource.test.ts

---
+++ src/infrastructure/datasources/mongo-log.datasource.test.ts
@@ -0,0 +1,47 @@
+import {envs} from '../../config/plugins/envs.plugin'
+import {LogModel, MongoDatabase} from '../../data/mongo'
+import mongoose from 'mongoose'
+import {MongoLogDatasource} from './mongo-log.datasource'
+import {LogEntity, LogSeverityLevel} from '../../domain/entities/log.entity'
+
+describe('MongoLogDatasource', () => {
+    const logDataSource = new MongoLogDatasource()
+    const log = new LogEntity({
+        level: LogSeverityLevel.medium,
+        message: 'test message',
+        origin: 'mongo-log.datasource.test.ts',
+    })
+
+    beforeAll(async () => {
+        await MongoDatabase.connect({
+            dbName: envs.MONGO_DB_NAME,
+            mongoUrl: envs.MONGO_URL,
+        })
+    })
+
+    afterEach(async () => {
+        await LogModel.deleteMany()
+    })
+
+    afterAll(() => {
+        mongoose.connection.close()
+    })
+
+    test('should create a log', async () => {
+        const logSpy = jest.spyOn(console, 'log')
+
+        await logDataSource.saveLog(log)
+
+        expect(logSpy).toHaveBeenCalled()
+        expect(logSpy).toHaveBeenCalledWith('Mongo Log created', expect.any(String))
+    })
+
+    test('should get logs', async () => {
+        await logDataSource.saveLog(log)
+
+        const logs = await logDataSource.getLogs(LogSeverityLevel.medium)
+
+        expect(logs).toHaveLength(1)
+        expect(logs[0].level).toBe(LogSeverityLevel.medium)
+    })
+})
