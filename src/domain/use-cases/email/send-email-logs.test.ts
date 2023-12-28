c56b9a5a3f63b0f74ba824e3b7666bcbad940c98
Chivan9406 <ivan9406@hotmail.com>
2023-12-27 20:32:01 -0600

    send-email-logs.test.ts

---
+++ src/domain/use-cases/email/send-email-logs.test.ts
@@ -0,0 +1,53 @@
+import {SendEmailLogs} from './send-email-logs'
+import {LogRepository} from '../../repository/log.repository'
+import {LogEntity} from '../../entities/log.entity'
+
+describe('SendEmailLogs', () => {
+    const mockEmailService = {
+        sendEmailWithFileSystemLogs: jest.fn().mockReturnValue(true)
+    }
+
+    const mockLogRepository: LogRepository = {
+        saveLog: jest.fn(),
+        getLogs: jest.fn(),
+    }
+
+    const sendEmailLogs = new SendEmailLogs(
+        mockEmailService as any,
+        mockLogRepository,
+    )
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test('should call sendEmail and saveLog', async () => {
+        const result = await sendEmailLogs.execute('chivan9406@gmail.com')
+
+        expect(result).toBe(true)
+        expect(mockEmailService.sendEmailWithFileSystemLogs).toHaveBeenCalledTimes(1)
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith(expect.any(LogEntity))
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith({
+            createdAt: expect.any(Date),
+            level: "low",
+            message: "Log email sent",
+            origin: "send-email-logs.ts",
+        })
+    })
+
+    test('should log in case of error', async () => {
+        mockEmailService.sendEmailWithFileSystemLogs.mockResolvedValue(false)
+
+        const result = await sendEmailLogs.execute('chivan9406@gmail.com')
+
+        expect(result).toBe(false);
+        expect(mockEmailService.sendEmailWithFileSystemLogs).toHaveBeenCalledTimes(1)
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith(expect.any(LogEntity))
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith({
+            createdAt: expect.any(Date),
+            level: "high",
+            message: "Error: Email log not sent",
+            origin: "send-email-logs.ts",
+        })
+    })
+})
