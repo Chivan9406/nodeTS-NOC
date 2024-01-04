abafde7faf298ca366bc130e348bb308d322888c
Chivan9406 <ivan9406@hotmail.com>
2024-01-03 18:03:46 -0600

    log.repository.impl.test.ts

---
+++ src/infrastructure/repositories/log.repository.impl.test.ts
@@ -0,0 +1,31 @@
+import {LogRepositoryImpl} from './log.repository.impl'
+import {LogEntity, LogSeverityLevel} from '../../domain/entities/log.entity'
+
+describe('LogRepositoryImpl', () => {
+    const mockLogDatasource = {
+        saveLog: jest.fn(),
+        getLogs: jest.fn(),
+    }
+
+    const logRepository = new LogRepositoryImpl(mockLogDatasource)
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test('saveLog should call the datasource with arguments', async () => {
+        const log = {level: LogSeverityLevel.high, message: 'mundo'} as LogEntity
+
+        await logRepository.saveLog(log)
+
+        expect(mockLogDatasource.saveLog).toHaveBeenCalledWith(log)
+    })
+
+    test('getLogs should call the datasource with arguments', async () => {
+        const lowSeverity = LogSeverityLevel.low
+
+        await logRepository.getLogs(lowSeverity)
+
+        expect(mockLogDatasource.getLogs).toHaveBeenCalledWith(lowSeverity)
+    })
+})
